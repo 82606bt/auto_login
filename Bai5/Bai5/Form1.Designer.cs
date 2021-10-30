b9fa50c04ad51997306833d2eb4b2640bee710db
unknown <doanlong266@gmail.com>
2021-10-30 12:52:23 +0700

    first commit

---
+++ Bai5/Bai5/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿
+namespace Bai5
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Username_textBox = new System.Windows.Forms.TextBox();
+            this.Password_TextBox = new System.Windows.Forms.TextBox();
+            this.Login_BTN = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(48, 78);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(110, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Tên đăng nhập:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(48, 164);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 20);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Mật khẩu:";
+            // 
+            // Username_textBox
+            // 
+            this.Username_textBox.Location = new System.Drawing.Point(48, 112);
+            this.Username_textBox.Name = "Username_textBox";
+            this.Username_textBox.Size = new System.Drawing.Size(202, 27);
+            this.Username_textBox.TabIndex = 1;
+            // 
+            // Password_TextBox
+            // 
+            this.Password_TextBox.Location = new System.Drawing.Point(48, 205);
+            this.Password_TextBox.Name = "Password_TextBox";
+            this.Password_TextBox.PasswordChar = '*';
+            this.Password_TextBox.Size = new System.Drawing.Size(202, 27);
+            this.Password_TextBox.TabIndex = 1;
+            this.Password_TextBox.TextChanged += new System.EventHandler(this.TxtBoxChanged);
+            // 
+            // Login_BTN
+            // 
+            this.Login_BTN.BackColor = System.Drawing.Color.Transparent;
+            this.Login_BTN.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.Login_BTN.ForeColor = System.Drawing.Color.Red;
+            this.Login_BTN.Location = new System.Drawing.Point(48, 267);
+            this.Login_BTN.Name = "Login_BTN";
+            this.Login_BTN.Size = new System.Drawing.Size(94, 29);
+            this.Login_BTN.TabIndex = 2;
+            this.Login_BTN.Text = "Đăng nhập";
+            this.Login_BTN.UseVisualStyleBackColor = false;
+            this.Login_BTN.Click += new System.EventHandler(this.Login_BTN_Click);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.Login_BTN;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(505, 450);
+            this.Controls.Add(this.Login_BTN);
+            this.Controls.Add(this.Password_TextBox);
+            this.Controls.Add(this.Username_textBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "DNTU Auto Login ";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox Username_textBox;
+        private System.Windows.Forms.TextBox Password_TextBox;
+        private System.Windows.Forms.Button Login_BTN;
+    }
+}
+
---
+++ Bai5/Bai5/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Threading;
+
+namespace Bai5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            CheckForIllegalCrossThreadCalls = false;
+        }
+    
+        ChromeDriver chr; Thread th; 
+
+        private void TxtBoxChanged(object sender, EventArgs e)
+        {
+            if(Username_textBox.Text != "" && Password_TextBox.Text != "")
+            {
+                Login_BTN.ForeColor = Color.Lime;
+                Login_BTN.Cursor = Cursors.Hand;
+            }
+            else
+            {
+                Login_BTN.ForeColor = Color.Red;
+                Login_BTN.Cursor = Cursors.No;
+            }
+        }
+
+        private void Login_BTN_Click(object sender, EventArgs e)
+        {
+            if(Login_BTN.Cursor == Cursors.Hand)
+            {
+                th = new Thread(Result); th.Start();
+
+            }
+        }
+        private void Result()
+        {
+            Login_BTN.ForeColor = Color.Gold;
+            Login_BTN.UseWaitCursor = true;
+            Login_BTN.Text = "Testing....";
+            OpenSelenium();
+            Thread.Sleep(3000);
+            Login(Username_textBox.Text, Password_TextBox.Text);
+            if (TestAccount())
+                MessageBox.Show("Đăng nhập thành công", "Thông tin", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            else
+                MessageBox.Show("Tài khoản hoặc mật khẩu sai", "Cảnh báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            CloseSelenium();
+            Login_BTN.ForeColor = Color.Lime;
+            Login_BTN.UseWaitCursor = false;
+            Login_BTN.Text = "Đăng nhập";
+
+        }
+
+        private void Login(string username, string password)
+        {
+            try
+            {
+                chr.FindElements(By.XPath("//input[@class='field text largue requi']"))[0].SendKeys(username); Thread.Sleep(3000);
+                chr.FindElements(By.XPath("//input[@class='field text largue requi']"))[1].SendKeys(password); Thread.Sleep(3000);
+                chr.FindElement(By.Id("_ctl18_butLogin")).Click(); Thread.Sleep(1000);
+            }
+            catch (Exception exc)
+
+            {
+                MessageBox.Show(exc.Message);
+            }
+
+            
+        }
+        private bool TestAccount()
+        {
+            if (chr.Url == "https://sv.dntu.edu.vn/home/newslist/bang-tin.htm")
+                return true;
+            else
+                return false;
+        }
+
+        private void OpenSelenium()
+        {
+            ChromeDriverService service = ChromeDriverService.CreateDefaultService();
+            service.HideCommandPromptWindow = true;
+            chr = new ChromeDriver(@"D:\Login_Selenium\chromedriver_win32");
+            chr.Navigate().GoToUrl("https://sv.dntu.edu.vn/login/dang-nhap.htm");
+
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            CloseSelenium();
+        }
+        private void CloseSelenium()
+        {
+            chr.Quit();
+        }
+    }
+}
